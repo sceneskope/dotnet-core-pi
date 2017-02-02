c29a0a585361cd90d3b992465c52d5ad9c09aa78
Nick Randell <nick@nickrandell.com>
2017-02-02 14:17:26 +0000

    Use new github library

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,5 +1,5 @@
-VERSION="1.2.0-beta-001291-00"
+VERSION="2.0.0-beta-001470-00"
 
 
-docker build -t sceneskope/armv7-dotnet .
+docker build --build-arg VERSION=$VERSION -t sceneskope/armv7-dotnet .
 docker tag sceneskope/armv7-dotnet sceneskope/armv7-dotnet:$VERSION
