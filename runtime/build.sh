4ea451687802877e6e90abd614c5e499f3ef0807
Nick Randell <nick@nickrandell.com>
2017-04-10 08:58:41 +0100

    Update from 1663 to 1915

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,6 +1,6 @@
 #VERSION=`curl -s https://dotnetcli.blob.core.windows.net/dotnet/master/dnvm/latest.sharedfx.ubuntu.16.04.arm.version | tr -d '\r' | tail -1`
 
-VERSION=2.0.0-beta-001663-00
+VERSION=2.0.0-beta-001915-00
 docker build --build-arg VERSION=$VERSION -t sceneskope/armv7-dotnet .
 docker tag sceneskope/armv7-dotnet sceneskope/armv7-dotnet:$VERSION
 docker push sceneskope/armv7-dotnet:$VERSION
