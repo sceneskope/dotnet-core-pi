6b3f47ba3d6720116a40d0332484fd9fdf103a46
Nick Randell <nick@nickrandell.com>
2017-06-05 06:56:22 +0100

    Use preview1 for 2.0.0

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,6 +1,6 @@
 #VERSION=`curl -s https://dotnetcli.blob.core.windows.net/dotnet/master/dnvm/latest.sharedfx.ubuntu.16.04.arm.version | tr -d '\r' | tail -1`
 
-VERSION=2.0.0-preview1-001915-00
-docker build --build-arg VERSION=$VERSION -t sceneskope/armv7-dotnet .
-docker tag sceneskope/armv7-dotnet sceneskope/armv7-dotnet:$VERSION
-docker push sceneskope/armv7-dotnet:$VERSION
+VERSION=2.0.0-preview1-002111-00
+docker build --build-arg VERSION=$VERSION -t sceneskope/arm32v7-dotnet .
+docker tag sceneskope/arm32v7-dotnet sceneskope/arm32v7-dotnet:$VERSION
+docker push sceneskope/arm32v7-dotnet:$VERSION
