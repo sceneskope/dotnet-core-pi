3be55dc0554deed43284ab3bba09fa275d858018
Nick Randell <nick@nickrandell.com>
2017-06-05 07:10:30 +0100

    Exit on error

--- runtime/build.sh
+++ runtime/build.sh
@@ -2,5 +2,10 @@
 
 VERSION=2.0.0-preview1-002111-00
 docker build --build-arg VERSION=$VERSION -t sceneskope/arm32v7-dotnet .
+if [ $? -ne 0 ]
+then
+    exit -1
+fi
+
 docker tag sceneskope/arm32v7-dotnet sceneskope/arm32v7-dotnet:$VERSION
 docker push sceneskope/arm32v7-dotnet:$VERSION
