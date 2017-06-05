2a1f185b846356f8d3688a796aea1d197011ce30
Nick Randell <nick@nickrandell.com>
2017-06-05 07:12:10 +0100

    Changed dotnet version arg

--- runtime/build.sh
+++ runtime/build.sh
@@ -1,7 +1,7 @@
 #VERSION=`curl -s https://dotnetcli.blob.core.windows.net/dotnet/master/dnvm/latest.sharedfx.ubuntu.16.04.arm.version | tr -d '\r' | tail -1`
 
-VERSION=2.0.0-preview1-002111-00
-docker build --build-arg VERSION=$VERSION -t sceneskope/arm32v7-dotnet .
+DOTNET_VERSION=2.0.0-preview1-002111-00
+docker build --build-arg DOTNET_VERSION=$DOTNET_VERSION -t sceneskope/arm32v7-dotnet .
 if [ $? -ne 0 ]
 then
     exit -1
